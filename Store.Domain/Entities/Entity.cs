5777711da673897629cc533d72a91bcbd5980822
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-06 19:29:31 -0300

    Creating Domain Layer with base entity

---
+++ Store.Domain/Entities/Entity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Store.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+    }
+}
