0b0a40a67b4e4f9cf5bea2adbed1c1450921d81f
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-07 12:47:20 -0300

    Added flunt to domain notifications

--- Store.Domain/Entities/Entity.cs
+++ Store.Domain/Entities/Entity.cs
@@ -1,8 +1,9 @@
 using System;
+using Flunt.Notifications;
 
 namespace Store.Domain.Entities
 {
-    public abstract class Entity
+    public abstract class Entity : Notifiable
     {
         public Entity()
         {
