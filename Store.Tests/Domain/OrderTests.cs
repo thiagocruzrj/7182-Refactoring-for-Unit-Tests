1ab33744b6732e6694a0716418e4659cb98faf20
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-07 15:19:05 -0300

    Ending all entities tests

--- Store.Tests/Domain/OrderTests.cs
+++ Store.Tests/Domain/OrderTests.cs
@@ -68,42 +68,54 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 0, null);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 50);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
+            var discont = new Discount(10, DateTime.Now.AddDays(-5));
+            var order = new Order(_customer, 10, discont);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 60);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
+            var order = new Order(_customer,0, null);
+            order.AddItem(_product, 6);
+            Assert.AreEqual(order.Total(), 60);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
         {
-            Assert.Fail();
+            var order = new Order(_customer,10, _discount);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 50);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
+            var order = new Order(_customer,10, _discount);
+            order.AddItem(_product, 6);
+            Assert.AreEqual(order.Total(), 60);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
         {
-            Assert.Fail();
+            var order = new Order(null, 10, _discount);
+            Assert.AreEqual(order.Invalid, true);
         }
     }
 }
